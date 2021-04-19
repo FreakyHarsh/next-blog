ac6206f8789f256441ae3a16f011112701d2d48f
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-04-19 16:30:42 +0530

    nimoor chlajgrew

--- redux/actions.ts
+++ redux/actions.ts
@@ -1,10 +1,12 @@
 import { Dispatch } from "redux";
 
 export const testAsync = () => async (dispatch: Dispatch, getState: any) => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(res => res.json()).then(data => {
-    dispatch({ type: 'INCREMENT' })
-    return data;
-  })
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(data => {
+      dispatch({ type: 'INCREMENT' })
+      return data;
+    })
   await new Promise(async (resolve, reject) => {
     console.log('THIS IS SET TIME OUT BROOO');
     await setTimeout((data) => {
